93faa36788b366bb6b00dec9171ac988a7784de5
Carlos J. Soto <csoto09@gmail.com>
2018-10-31 11:37:01 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -4,23 +4,26 @@ import Landing from "./components/Landing";
 import Library from './components/Library';
 import Album from './components/Album'
 import './App.css';
+import './../node_modules/bootstrap/dist/css/bootstrap.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header>
+      <div className="App container">
+        <div className="row">
+        <aside className="col-sm">
           <nav>
             <Link to='/'>Landing</Link>
             <Link to='/library'>Library</Link>
           </nav>
           <h1>Bloc Jams</h1>
-        </header>
-        <main>
+        </aside>
+        <main className="col-lg ">
           <Route exact path="/" component={Landing} />
           <Route path="/library" component={Library} />
           <Route path="/album/:slug" component={Album} />
         </main>
+        </div>
       </div>
     );
   }
